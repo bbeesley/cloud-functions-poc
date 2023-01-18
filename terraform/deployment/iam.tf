f95b46d307031f994b59637600bad20929bd6787
Bill Beesley <bill.beesley@soundcloud.com>
2023-01-18 10:24:55 +0000

    ci: add log writer permissions to fortune

---
+++ terraform/deployment/iam.tf
@@ -0,0 +1,22 @@
+locals {
+  api_service_account_id     = "${var.service_name_short}-api-sc-${var.environment}"
+  fortune_service_account_id = "${var.service_name_short}-fortune-sc-${var.environment}"
+}
+
+resource "google_service_account" "api_account" {
+  account_id   = local.api_service_account_id
+  display_name = "Service account for the cloud fn api"
+}
+
+resource "google_service_account" "fortune_account" {
+  account_id   = local.fortune_service_account_id
+  display_name = "Service account for the fortune api"
+}
+
+resource "google_service_account_iam_binding" "fortune_account_logging" {
+  service_account_id = google_service_account.fortune_account.name
+  role               = "roles/logging.logWriter"
+  members = [
+    "allUsers",
+  ]
+}
